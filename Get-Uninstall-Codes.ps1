a0e326c85d081d53ea6bad7f861f12ac8cbcb420
BHABHI <45754005+NiranjanLakra@users.noreply.github.com>
2020-10-09 05:29:53 +0530

    Add files via upload
    Helps getting uninstall strings

---
+++ Get-Uninstall-Codes.ps1
@@ -0,0 +1,37 @@
+﻿param($productName)
+try{
+    Write-Host “------------------------Getting UninstallString, Products Codes------------------------”
+    Write-Host " "
+    Write-Host “HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\”
+    Get-childitem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Select-Object -Property DisplayName, UninstallString | Select-String $productName
+    Write-Host " "
+    Write-Host “--------------------------------------------------------------------------”
+    Write-Host " "
+    Write-Host “HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\”
+    Write-Host " "
+    Get-childitem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\  | Get-ItemProperty | Select-Object -Property DisplayName, UninstallString | Select-String $productName
+    Write-Host " "
+    Write-Host “--------------------------------------------------------------------------”
+    $a=Get-PSDrive |Select-String HKCR
+    if ($a -like ‘HKCR’)
+        {
+            Write-Host " "
+            Write-Host “HKCR already Mounted”
+            Write-Host “--------------------------------------------------------------------------”
+        }
+    else
+        {
+            Write-Host " "
+            Write-Host “[+] Mounting HKCR”
+            New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -NAme HKCR
+            Write-Host “[+] HKCR Mounted”
+            Write-Host " "
+            Write-Host “--------------------------------------------------------------------------”
+        }
+    Get-childitem -Path Registry::HKEY_CLASSES_ROOT\Installer\Products\ | Get-ItemProperty | Select-Object -Property ProductName, PackageCode, ProductIcon | Select-String $productName
+    Remove-PSDrive -Name HKCR
+    Write-Host " "
+    Write-Host “[-] UN-Mounted HKCR”
+    Write-Host " "
+    }
+catch{"an error occured"}
